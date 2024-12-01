0409c64ff95567c9fbce9200687dfa8944a83731
EZ3ki33l <r.rousset31@gmail.com>
2024-12-01 11:09:54 +0100

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    appDir: true,
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      DIRECT_URL: process.env.DIRECT_URL,
+      UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
+    },
     serverActions: {
       mode: "default", // Exemple, ajustez selon vos besoins
     },
